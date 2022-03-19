499e8369209dd3c17c2320ad53cb16ca7526bbf3
Fahimmuntasir1 <fahimmuntasir043@gmail.comm>
2022-03-19 18:18:12 +0600

    api changed to comment

--- src/Compnonents/Overwrite/ToDo.js
+++ src/Compnonents/Overwrite/ToDo.js
@@ -1,32 +1,35 @@
 import React, { useEffect, useState } from "react";
+import "./ToDo.css";
 
 const ToDo = (props) => {
-  const [users, setUser] = useState([]);
-//   console.log(users);
+  const [comments, setComments] = useState([]);
+  //   console.log(users);
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/todos")
+    fetch("https://jsonplaceholder.typicode.com/comments")
       .then((res) => res.json())
-      .then((data) => setUser(data));
+      .then((data) => setComments(data));
   }, []);
   return (
     <div>
       {/* <h2>{props.heading}</h2>
       <h3>{props.author}</h3> */}
 
-      <h2>All Users {users.length}</h2>
-      {users.map((user) => (
-        <User user={user} key={user.id}></User>
+      <h2>Available Comments {comments.length}</h2>
+      {comments.map((comment) => (
+        <User comment={comment} key={comment.id}></User>
       ))}
     </div>
   );
 };
 
 const User = (props) => {
-    // console.log(props);
+  console.log(props);
   return (
-    <div>
-      <h2>Title: {props.user.title}</h2>
-      <h3>hello</h3>
+    <div className="todo-style">
+      <p>id:{props.comment.id}</p>
+      <p className="name">UserName: {props.comment.name}</p>
+      <p className="email">Email: {props.comment.email}</p>
+      <p className="comment"> {props.comment.body}</p>
     </div>
   );
 };
